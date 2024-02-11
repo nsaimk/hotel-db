370c4c1b1aa3fef4e90af3a9b336c15e95cd5fc0
nsaimk <nsaimkorkmaz1@gmail.com>
2024-02-11 18:24:03 +0000

    get by id update

--- server.js
+++ server.js
@@ -21,6 +21,21 @@ app.get('/', async (req, res) => {
     }
 });
 
+app.get("/:id", async (req, res) => {
+    const id = req.params.id;
+   
+    try {
+        const allItems = await itemsPool.query(
+            'SELECT * FROM bookings WHERE id = $1', [id]
+        );
+        const rows = allItems.rows;
+        res.json({ rows });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message);
+    }
+});
+
 
 console.log('Using port:', PORT); // Debugging statement
 
